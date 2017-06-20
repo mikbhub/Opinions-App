2c93f33ad3301c742ba5013de4cc1cfec70755db
mikbhub <mikolaj.babiak@gmail.com>
2017-06-20 22:57:55 +0200

    re-organise repo

--- angry_customers/feedback_form_service/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . views import FeebackForm
-
-app_name = 'feedback_form_service'
-
-urlpatterns = [
-    url(r'^post-feedback/$', FeebackForm.as_view(), name='post-feedback'),
-
-]
