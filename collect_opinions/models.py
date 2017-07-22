2d7949aa02231f925c9528a04d3b0cde56e340f1
mikbhub <mikolaj.babiak@gmail.com>
2017-07-22 22:14:30 +0200

    FeedbackManager is now responsible for creating Feedback instances and relating them to Customers

--- collect_opinions/models.py
+++ collect_opinions/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 
-# Create your models here.
 class Customer(models.Model):
+    """
+    Customers share their opinions on the web, in social media
+    and via contact forms.
+    """
 
     email = models.EmailField(max_length=128, unique=True, null=True, db_index=True)
     name = models.CharField(max_length=128)
@@ -15,13 +18,39 @@ class Customer(models.Model):
         verbose_name_plural = 'Customers'
 
 
+class FeedbackManager(models.Manager):
+    """
+    Encapsulates Feedback creation and updating.
+    """
+    
+    # TODO: implement 'create' method that creates or updates customer instance realted to feedback instance
+    def create_feedback_from_Form_or_Api(self, email, name, **feedback_kwargs):
+        """
+        Creates new Feedback instance and assigns it to Customer from the database.
+        If customer is not in the database, creates Customer instance and saves it to the database.
+        """
+        customer, created = Customer.objects.update_or_create(
+                email=email,
+                defaults={'name': name},
+            )
+        feedback_kwargs.update({'customer': customer})
+        feedback = Feedback.objects.create(**feedback_kwargs)
+        return feedback
+
+
 class Feedback(models.Model):
+    """
+    Opinion shared by a customer.
+    """
 
     text = models.TextField()
     date = models.DateTimeField(auto_now=True)
     customer = models.ForeignKey(to=Customer, related_name="feedbacks_send")
-    source_type = models.CharField(max_length=128, null=True)
-    source_url = models.CharField(max_length=400, null=True)
+    source_type = models.CharField(max_length=128, null=True)  # contact form, facebook, twitter etc.
+    source_url = models.CharField(max_length=400, null=True)  # internet location
+    location = models.CharField(max_length=60, null=True, blank=True)  # geolocation
+
+    objects = FeedbackManager()
 
     @property
     def short_text(self):
