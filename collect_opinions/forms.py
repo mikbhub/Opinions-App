2af75ed48ea32288140811a83f65e991bb9916ac
mikbhub <mikolaj.babiak@gmail.com>
2017-08-07 17:54:13 +0200

    Removed old commented code.

--- collect_opinions/forms.py
+++ collect_opinions/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from . models import Feedback
-# from django.utils.translation import ugettext_lazy as _
 
 
 class FeedbackForm(forms.Form):
@@ -8,28 +7,3 @@ class FeedbackForm(forms.Form):
     name = forms.CharField(max_length=128)
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
-
-
-# class FeedbackForm(forms.ModelForm):
-
-#     # customer = forms.EmailField()
-
-#     class Meta:
-#         model = Feedback
-#         fields = (
-#             'title',
-#             'description',
-#             'customer',
-#         )
-
-#         labels = {
-#             'customer': _('Your email'),
-#         }
-
-#         help_texts = {
-#             'description': _('Please describe your problem.'),
-#         }
-
-#         # widgets = {
-#         #     'customer': forms.EmailInput(),
-#         # }
