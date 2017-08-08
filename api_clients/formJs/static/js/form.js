2071eb286d62edb2d44e95819cb8cb64c94e7dcd
mikbhub <mikolaj.babiak@gmail.com>
2017-08-08 21:36:31 +0200

    Altered json data to include firs_name and last_name. Removed console.logs

--- api_clients/formJs/static/js/form.js
+++ api_clients/formJs/static/js/form.js
@@ -2,29 +2,29 @@
 
 // ajax app
 $(document).ready(function () {
-    console.log('script loaded');
 
     let endpoint = 'http://127.0.0.1:8110/collect_opinions/api/feedbacks/new/';
     let form = document.forms.feedbackform;
 
     // set notification timeout in microseconds. 1000 ms = 1 s
-    let notification_timeout = 6000;
+    let notification_timeout = 4000;
 
     $(form).submit(function (event) {
         event.preventDefault();
-        Materialize.toast('Sending!', notification_timeout);;
+        Materialize.toast('Sending!', notification_timeout);
         let request = $.ajax({
             type: 'POST',
             contentType: 'application/json',
             url: endpoint,
             data: JSON.stringify({
-                "customer": {
-                    "email": form.email.value,
-                    "name": form.name.value
+                'customer': {
+                    'email': form.email.value,
+                    'first_name': form.first_name.value,
+                    'last_name': form.last_name.value
                 },
-                "text": form.text.value,
-                "source_type": "formjs",
-                "source_url": "http://127.0.0.1:8120/"
+                'text': form.text.value,
+                'source_type': 'formjs',
+                'source_url': 'http://127.0.0.1:8120/'
             }),
             dataType: 'json',
         });
@@ -32,15 +32,12 @@ $(document).ready(function () {
             // Materialize.toast(message, displayLength, className, completeCallback);
             Materialize.toast('Thank you for your opinion!', notification_timeout, 'green'); // 4000 is the duration of the toast
             // alert('Thank you for your opinion.');
-            $("button").prop('disabled', true);
+            $('button').prop('disabled', true);
         });
         request.fail(function (xhr, status, errorThrown) {
             Materialize.toast(`Sorry, there was a problem!: ${errorThrown} Status: ${xhr.status}`, notification_timeout, 'red darken-1'); // 4000 is the duration of the toast
             let json_error = xhr.responseText;
             Materialize.toast(`Response: ${json_error}`, notification_timeout); // 4000 is the duration of the toast
-            console.log("Error: " + errorThrown);
-            console.log("Status: " + status);
-            console.dir(xhr);
         });
     });
 });
