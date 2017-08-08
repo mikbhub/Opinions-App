4b2ec2999db4d1176c558bed82407294bf1adeee
mikbhub <mikolaj.babiak@gmail.com>
2017-08-08 21:53:09 +0200

    Replaced name field with first_name. Added last_name field.

--- collect_opinions/forms.py
+++ collect_opinions/forms.py
@@ -4,6 +4,7 @@ from . models import Feedback
 
 class FeedbackForm(forms.Form):
 
-    name = forms.CharField(max_length=128)
+    first_name = forms.CharField(max_length=128)
+    last_name = forms.CharField(max_length=128)
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
