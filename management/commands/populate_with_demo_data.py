73497d7907a0d6648c20232ace08a40faea57c03
mikbhub <mikolaj.babiak@gmail.com>
2017-08-09 09:28:17 +0200

    Script to restore database to demo state.

---
+++ management/commands/populate_with_demo_data.py
@@ -0,0 +1,11 @@
+def make_demo():
+    """
+    Deletes user content from the database and fills the database with demo data.
+    """
+    raise NotImplementedError
+
+def main():
+    make_demo()
+
+if __name__ == '__main__':
+    main()
